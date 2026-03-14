af348a85fbbdb81af722e25ce48a1936d8f6f2c4
jonny cau <pjonny@gmail.com>
2026-03-14 18:17:49 +0100

    Update AgentsBoard to v0.3.0

--- Casks/agentsboard.rb
+++ Casks/agentsboard.rb
@@ -1,8 +1,8 @@
 # MARK: - Homebrew Cask Formula (Step 20.1)
 
 cask "agentsboard" do
-  version "0.2.0"
-  sha256 "3037919937432e29e477991e0e05824ed7626d57234c5c65ee881407376b3fe7"
+  version "0.3.0"
+  sha256 "5930f88b71eabe2644fa653e31d27419ca1fb025bd5e9469ba4cd51049c2f9ba"
 
   url "https://github.com/pjcau/AgentsBoard/releases/download/v#{version}/AgentsBoard-#{version}.dmg"
   name "AgentsBoard"
